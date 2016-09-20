be75b47281244646355a614bfec61faa7e579a06
John <John@JOHN>
2016-09-20 09:04:30 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Timer
+{
+	class Timer
+	{
+		public int Seconds = 0;
+
+		public override string ToString()
+		{
+			TimeSpan t = TimeSpan.FromSeconds(Seconds);
+			return string.Format("{0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds);
+		}
+	}
+}
